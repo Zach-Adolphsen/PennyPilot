6daee3c54df9dcd789e78775171a9e0eaa120645
Zach-Adolphsen <zladolphsen@gmail.com>
2025-04-24 12:55:38 -0500

    added Income and Expense Components and Income and Expense Services

---
+++ src/app/Components/income-card/income-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-income-card',
+  imports: [],
+  templateUrl: './income-card.component.html',
+  styleUrl: './income-card.component.css'
+})
+export class IncomeCardComponent {
+
+}
