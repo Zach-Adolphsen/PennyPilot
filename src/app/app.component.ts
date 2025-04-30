05a998c09267d398fbd87caa84d192a35fe1bd21
ryanwetzstein <ryan.wetzstein@ndus.edu>
2025-04-30 15:37:53 -0500

    began dashboard component implementation, routing is WIP and no real data is in effect at the moment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './Components/navbar/navbar.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { IncomeCardComponent } from './Components/income-card/income-card.component';
+import { ExpenseCardComponent } from './Components/expense-card/expense-card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {path: 'income', component: IncomeCardComponent} ,
+   {path: 'expenses', component: ExpenseCardComponent} ,
+];
