c443435f6ca129ecfeb4a82e74bba682fe54707e
ryanwetzstein <ryan.wetzstein@ndus.edu>
2025-05-02 13:52:16 -0500

    added auth and updated angular version to 19.2.9

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,25 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 import { routes } from './app.routes';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDJtLkCGW7G9pjQ6Db87MeiC0vTGBfZtmo',
+  authDomain: 'pennypilot-70751.firebaseapp.com',
+  projectId: 'pennypilot-70751',
+  storageBucket: 'pennypilot-70751.firebasestorage.app',
+  messagingSenderId: '442614312413',
+  appId: '1:442614312413:web:2075d3992e599337b01a34',
+  measurementId: 'G-6G66SF10MS',
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth())
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
-import { AccountComponent } from './Components/account/account.component';
+import { AccountComponent } from './Components/auth/account/account.component';
 import { IncomePageComponent } from './income-page/income-page.component';
 import { ExpensePageComponent } from './expense-page/expense-page.component';
+import { LoginComponent } from './Components/auth/login/login.component';
+import { RegisterComponent } from './Components/auth/register/register.component';
+import { VerifyEmailComponent } from './Components/auth/verify-email/verify-email.component';
+import { ForgotPasswordComponent } from './Components/auth/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -10,4 +14,21 @@ export const routes: Routes = [
   { path: 'account', component: AccountComponent },
   { path: 'income', component: IncomePageComponent },
   { path: 'expense', component: ExpensePageComponent },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent,
+  },
+  {
+    path: 'verify-email',
+    component: VerifyEmailComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Register',
+  },
 ];
