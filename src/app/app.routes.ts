64e89e0f5f9d7f9e537d7ab6cf5ce41e0281e775
Zach-Adolphsen <zladolphsen@gmail.com>
2025-05-06 11:28:13 -0500

    added account-page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,17 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
-import { AccountComponent } from './Components/auth/account/account.component';
 import { IncomePageComponent } from './Components/income-page/income-page.component';
 import { ExpensePageComponent } from './Components/expense-page/expense-page.component';
 import { LoginComponent } from './Components/auth/login/login.component';
 import { RegisterComponent } from './Components/auth/register/register.component';
 import { VerifyEmailComponent } from './Components/auth/verify-email/verify-email.component';
 import { ForgotPasswordComponent } from './Components/auth/forgot-password/forgot-password.component';
+import { AccountPageComponent } from './Components/account-page/account-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', component: DashboardComponent },
-  { path: 'account', component: AccountComponent },
+  { path: 'account', component: AccountPageComponent },
   { path: 'income', component: IncomePageComponent },
   { path: 'expense', component: ExpensePageComponent },
   {
